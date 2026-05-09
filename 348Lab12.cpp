9cf08cdd0a6dda54d9bc0401264e3861a9d2a99b
MaddiClark <cclar@ku.edu>
2026-05-08 22:25:52 -0500

    Added finding the start of a possible number

--- 348Lab12.cpp
+++ 348Lab12.cpp
@@ -13,9 +13,27 @@
 using namespace std;
 
 double extractNumeric(const string& str) {
+    for (int i = 0; i < str.length(); i++) {
+        if (isdigit(str[i]) || str[i] == '+' || str[i] == '-' || str[i] == '.') {
+            if ((str[i] == '+' || str[i] == '-')) {
+                if (i + 1 >= n){
+                    continue;
+                }
 
+                if (!isdigit(str[i + 1]) && str[i + 1] != '.') {
+                    continue;
+                }
+            }
+
+            int j = i;
+
+            while (j < n && (isdigit(str[j]) || str[j] == '+' || str[j] == '-' || str[j] == '.' || str[j] == 'e' || str[j] == 'E')) {
+                j++;
+            }
+        }
+    }
 }
 
 void main() {
-    extractNumeric(string)
+    extractNumeric(string);
 }
