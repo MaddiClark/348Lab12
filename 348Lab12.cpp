c092d4cdd490976329b6eecd6e453e03d82666b4
MaddiClark <cclar@ku.edu>
2026-05-08 22:17:28 -0500

    Added required libraries and shells of functions

---
+++ 348Lab12.cpp
@@ -0,0 +1,21 @@
+//Name: Maddi Clark
+//ID: 3162616
+//Date: 05/08/2026
+//Description: extracts numbers from an inputted string of text
+//Inputs: a string of garbage characters with a number to be extracted
+//Outputs: the extracted number
+
+#include <iostream> //Allows for input and output streams
+#include <string> //Allows the use of string data type
+#include <cctype> //Allows for functions isdigit() and isalpha()
+#include <cmath> //Allows for math function pow()
+
+using namespace std;
+
+double extractNumeric(const string& str) {
+
+}
+
+void main() {
+    extractNumeric(string)
+}
