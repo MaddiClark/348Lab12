92a6e64a2fb86fa7fe01b9cb9243c0e886576839
MaddiClark <cclar@ku.edu>
2026-05-08 22:31:12 -0500

    Added inline comments to possible number beginning stage

--- 348Lab12.cpp
+++ 348Lab12.cpp
@@ -13,22 +13,23 @@
 using namespace std;
 
 double extractNumeric(const string& str) {
-    for (int i = 0; i < str.length(); i++) {
-        if (isdigit(str[i]) || str[i] == '+' || str[i] == '-' || str[i] == '.') {
-            if ((str[i] == '+' || str[i] == '-')) {
-                if (i + 1 >= n){
-                    continue;
-                }
+    int n = str.length(); //assigns string length a variable for later use
 
-                if (!isdigit(str[i + 1]) && str[i + 1] != '.') {
+    for (int i = 0; i < str.length(); i++) {
+        if (isdigit(str[i]) || str[i] == '+' || str[i] == '-' || str[i] == '.') { //checks if index is a digit or possible unary operator
+            if ((str[i] == '+' || str[i] == '-')) {
+                if (i + 1 >= n){ //checks if a sign is at the end of the string
+                    continue;
+
+                if (!isdigit(str[i + 1]) && str[i + 1] != '.') { //checks if the sign is followed by a non-digit that isnt a decimal point
                     continue;
                 }
             }
 
             int j = i;
 
-            while (j < n && (isdigit(str[j]) || str[j] == '+' || str[j] == '-' || str[j] == '.' || str[j] == 'e' || str[j] == 'E')) {
-                j++;
+            while (j < n && (isdigit(str[j]) || str[j] == '+' || str[j] == '-' || str[j] == '.' || str[j] == 'e' || str[j] == 'E')) { 
+                j++; //increments j for as long as it is less than the length of the string and is a possible piece of a number
             }
         }
     }
